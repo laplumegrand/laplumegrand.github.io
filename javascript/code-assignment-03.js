9c4f5dbc8af59fb82a462da3d53005f5e8126901
laplumegrand <35343379+laplumegrand@users.noreply.github.com>
2018-02-15 11:36:28 -0600

    consol log stuff

--- javascript/code-assignment-03.js
+++ javascript/code-assignment-03.js
@@ -1,3 +1,9 @@
 let projectId = 1
 let projectTitle = "Earthquakes"
-let project
+let projectBoolean = false
+let projectMapExtent = [48, -27.5, 5.5, -125.3]
+
+console.log(projectId);
+console.log(projectTitle);
+console.log(projectBoolean);
+console.log(projectMapExtent);
