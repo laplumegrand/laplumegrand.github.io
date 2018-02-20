86c3079cdf18faea1e179a987572c061aac860cd
laplumegrand <35343379+laplumegrand@users.noreply.github.com>
2018-02-20 10:31:28 -0600

    up

--- javascript/code-assignment-03.js
+++ javascript/code-assignment-03.js
@@ -3,7 +3,7 @@ let projectTitle = "Earthquakes"
 let projectBoolean = false
 let projectMapExtent = [48, -27.5, 5.5, -125.3]
 
-console.log(projectId);
-console.log(projectTitle);
-console.log(projectBoolean);
-console.log(projectMapExtent);
+console.log(projectId +" This is a variable that is a number");
+console.log(projectTitle+" This is a variable that is a string");
+console.log(projectBoolean+ " This is a variable the is a boolean");
+console.log(projectMapExtent +" This is a variable that is an array");
