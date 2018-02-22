3dd9cecfd59b2f85a7e8479c457c7e9397d29ee7
laplumegrand <35343379+laplumegrand@users.noreply.github.com>
2018-02-22 11:18:51 -0600

    class pt. 2 update

---
+++ javascript/code-assignment-04.js
@@ -0,0 +1,24 @@
+let projectId = 1
+let projectTitle = "Assignment 1"
+let projectDesc = "Some text, then a hyperlink about GIS info on Baton Rouge."
+let projectIMG = "?"
+let projectURL = "https://data-ebrgis.opendata.arcgis.com"
+
+let projectArray = [
+  projectId,
+  projectTitle,
+  projectDesc,
+  projectIMG,
+  projectURL
+]
+
+
+let projectObject {
+  id: projectId
+  title: projectTitle,
+  desc: projectDesc,
+  img: projectIMG,
+  url: projectURL,
+}
+
+console.log(projectObject.URL);
