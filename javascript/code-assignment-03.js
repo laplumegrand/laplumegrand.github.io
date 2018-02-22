ed626227197fbe3b0e1f417758fda3ade6c77080
laplumegrand <35343379+laplumegrand@users.noreply.github.com>
2018-02-22 11:02:15 -0600

    javascript update real

--- javascript/code-assignment-03.js
+++ javascript/code-assignment-03.js
@@ -1,9 +1,9 @@
-let projectId = 1
-let projectTitle = "Earthquakes"
+let projectId = 100
+let projectTitle = "Console Functions with Javascript"
 let projectBoolean = false
-let projectMapExtent = [48, -27.5, 5.5, -125.3]
+let projectMapExtent = ["purple", -27.5, false, "gold"]
 
 console.log(projectId +" This is a variable that is a number");
 console.log(projectTitle+" This is a variable that is a string");
 console.log(projectBoolean+ " This is a variable the is a boolean");
-console.log(projectMapExtent +" This is a variable that is an array");
+console.log(projectMapExtent +" This is a variable that is also an array");
