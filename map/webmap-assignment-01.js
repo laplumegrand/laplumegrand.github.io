f928728430cdbe781d01a2be473faa851070e9d4
laplumegrand <lafeather1825@gmail.com>
2018-04-17 10:50:09 -0500

    polyline

--- map/webmap-assignment-01.js
+++ map/webmap-assignment-01.js
@@ -13,7 +13,35 @@ let polygon = L.polygon([
   [30.440066, -91.179530],
   [30.458401, -91.180856],
   [30.458013, -91.189962]
-]).addTo(mymap);
+]).addTo(webmap);
+
+var polylinePoints = [
+            new L.LatLng(30.408893, -91.170057),
+            new L.LatLng(30.407773, -91.169034),
+            new L.LatLng(30.407908, -91.168128),
+            new L.LatLng(30.407535, -91.166869),
+            new L.LatLng(30.407840, -91.166200),
+            new L.LatLng(30.407840, -91.165885),
+            new L.LatLng(30.407331, -91.164744),
+            new L.LatLng(30.409979, -91.162264),
+            new L.LatLng(30.410997, -91.163563),
+            new L.LatLng(30.411676, -91.163721),
+            new L.LatLng(30.414018, -91.162776),
+            new L.LatLng(30.414730, -91.162737),
+            new L.LatLng(30.414493, -91.162264),
+            new L.LatLng(30.412694, -91.159588)
+         ];
+
+var polylineOptions = {
+                       color: 'blue',
+                       weight: 6,
+                       opacity: 0.9
+                     };
+
+var polyline = new L.Polyline(polylinePoints, polylineOptions);
+
+webmap.addLayer(polyline);
+
 
 polygon.bindPopup('Downtown Baton Rouge');
 marker.bindPopup('LSU Lakes/BREC Park');
@@ -21,4 +49,5 @@ marker.bindPopup('LSU Lakes/BREC Park');
 function logCurrentCoordinates (event) {
   console.log('You clicked the map at ' + event.latlng)
 }
-mymap.on('click', logCurrentCoordinates);
+
+webmap.on('click', logCurrentCoordinates);
