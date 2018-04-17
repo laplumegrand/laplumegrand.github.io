500971bad80641da23051d830a5a3ef975ed31ac
laplumegrand <lafeather1825@gmail.com>
2018-04-17 11:11:39 -0500

    first webmap

--- map/webmap-assignment-01.js
+++ map/webmap-assignment-01.js
@@ -1,9 +1,10 @@
-var webmap = L.map('mapid').setView([30.441221, -91.186152], 10);
+var webmap = L.map('mapid').setView([30.441221, -91.186152], 12);
 
-L.tileLayer('https://{s}.tile.thunderforest.com/landscape/{z}/{x}/{y}.png?apikey={apikey}', {
-	attribution: '&copy; <a href="http://www.thunderforest.com/">Thunderforest</a>, &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-	apikey: '<your apikey>',
-	maxZoom: 22
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox.streets',
+    accessToken: 'pk.eyJ1IjoibGFwbHVtZSIsImEiOiJjamczdXpqankwNXFlMnd0NW1yOGhodzhiIn0.s0Lgw6ScO583Nrj4Smmr6w'
 }).addTo(webmap);
 
 let marker = L.marker([30.431246, -91.168401]).addTo(webmap);
