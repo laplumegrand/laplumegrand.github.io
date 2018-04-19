fa2bba45a2705d6783ce4c56baec9aea50defc2a
laplumegrand <lafeather1825@gmail.com>
2018-04-19 10:40:20 -0500

    webmap 3

--- map/webmap-assignment-03.js
+++ map/webmap-assignment-03.js
@@ -4,8 +4,27 @@ let myPawprint = L.icon({
   iconSize: [30, 30], // size of the icon
   iconAnchor: [15, 15 ], // point of the icon which will correspond to marker's location
   popupAnchor: [0,-13] // point from which the popup should open relative to the iconAnchor
-})
+});
 
 let newMarker = L.marker([30.424231, -91.16035], {icon: myPawprint}).addTo(webmap);
 
 newMarker.bindPopup('Animal Sighting');
+
+let coordinates = [
+  [30.418276, -91.194283],
+  [30.419056, -91.170246],
+  [30.409438, -91.166403],
+  [30.408854, -91.170020],
+  [30.403395, -91.175144],
+  [30.404175, -91.183357],
+  [30.397221, -91.178685],
+  [30.397416, -91.203927]
+];
+
+let style = {
+  color: 'purple',
+  fillColor: 'gold',
+  fillOpacity: 0.16
+};
+
+let newPolygon = L.polygon(coordinates, style).addTo(webmap);
